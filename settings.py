9a4d6979101def4352ae47595acc63fd953aef52
Vasilii <basswan@yandex.ru>
2017-08-18 16:38:21 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -3,3 +3,7 @@ min_volume = 0.1  # min volume
 min_ask = 0.000005  # min ask
 min_profit = 0.04
 max_profit = 0.25
+min_delta = 0.04
+min_order_mult = 1.4
+buy_step = 0.00000001
+sell_step = 0.00000001
