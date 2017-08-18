5ceeeedee21bd7be6b5b4b4899094c15336f687b
Vasilii <basswan@yandex.ru>
2017-08-18 16:43:13 +0300

    Add files via upload

--- livecoin_api/tradeapi.py
+++ livecoin_api/tradeapi.py
@@ -1,10 +1,21 @@
 from basebot import abstract_api
 import requests
 import json
+import hmac
+import hashlib
+from livecoin_api.skeys import client_key
+from livecoin_api.skeys import client_secret
+from urllib.parse import urlencode
+from collections import OrderedDict
+import http.client
 
 ex_url = 'https://api.livecoin.net/'
+server = 'api.livecoin.net'
 
 proxy = 'proxy.ru:3128'
+proxyhost = 'proxy.ru'
+proxyport = 3128
+proxy_used = True
 
 proxyDict = {
     "http": proxy,
@@ -19,6 +30,35 @@ def tr_query(url):
 
 
 class trade_api(abstract_api):
+    def get_request(self, method, data):
+        encoded_data = urlencode(data)
+        sign = hmac.new(client_secret.encode(), msg=encoded_data.encode(), digestmod=hashlib.sha256).hexdigest().upper()
+        headers = {"Api-key": client_key, "Sign": sign}
+        if proxy_used:
+            conn = http.client.HTTPSConnection(proxyhost, proxyport)
+            conn.set_tunnel(server)
+        else:
+            conn = http.client.HTTPSConnection(server)
+        conn.request("GET", method + '?' + encoded_data, '', headers)
+        response = conn.getresponse().read().decode('utf-8')
+        conn.close()
+        return response
+
+    def post_request(self, method, data):
+        encoded_data = urlencode(data)
+        sign = hmac.new(client_secret.encode(), msg=encoded_data.encode(),
+                        digestmod=hashlib.sha256).hexdigest().upper()
+        headers = {"Api-key": client_key, "Sign": sign, "Content-type": "application/x-www-form-urlencoded"}
+        if proxy_used:
+            conn = http.client.HTTPSConnection(proxyhost, proxyport)
+            conn.set_tunnel(server)
+        else:
+            conn = http.client.HTTPSConnection(server)
+        conn.request("POST", method, encoded_data, headers)
+        response = conn.getresponse().read().decode('utf-8')
+        conn.close()
+        return response
+
     def clear_orders(self):
         pass
 
@@ -44,10 +84,104 @@ class trade_api(abstract_api):
         return 'errorCode' in req
 
     def get_currency_info(self, req):
-       return req['currencyPairs'][0]
+        return req['currencyPairs'][0]
 
     def get_minask(self, pair):
         return float(pair['minAsk'])
 
     def get_maxbid(self, pair):
         return float(pair['maxBid'])
+
+    def get_balanses(self):
+        method = '/payment/balances'
+        data = OrderedDict([])
+        response = self.get_request(method, data)
+        d = json.loads(response)
+        balances = {}
+        for cu in d:
+            if ('None' not in str(cu['value'])):
+                if (cu['value'] > 0):
+                    if cu['currency'] not in balances:
+                        balances[cu['currency']] = cu['value']
+
+        return balances
+
+    def get_openorders(self):
+        method = '/exchange/client_orders'
+        data = OrderedDict(sorted([('openClosed', 'OPEN')]))
+        response = self.get_request(method, data)
+        d = json.loads(response)
+        order_ids = []
+        if d['totalRows'] > 0:
+            d = d['data']
+            for od in d:
+                if od['orderStatus'] == 'OPEN':
+                    order_ids.append({'pair': od['currencyPair'], 'id': od['id']})
+        return order_ids
+
+    def get_partiallyorders(self):
+        method = '/exchange/client_orders'
+        data = OrderedDict(sorted([('openClosed', 'PARTIALLY')]))
+        response = self.get_request(method, data)
+        d = json.loads(response)
+        order_pairs = []
+        if d['totalRows'] > 0:
+            d = d['data']
+            for od in d:
+                if od['orderStatus'] == 'PARTIALLY':
+                    order_pairs.append(od['currencyPair'])
+        return order_pairs
+
+    def get_buy_price(self, pair):
+        '''
+            Выдать цену покупки имеющеёся вылюты
+        :param pair:
+        :return: float
+        '''
+        method = '/exchange/trades'
+        data = OrderedDict(sorted([('currencyPair', pair)]))
+        response = self.get_request(method, data)
+        d = json.loads(response)
+        if 'success' in d:
+            if d['success'] == False:
+                return 0
+        sum = 0
+        for tr in d:
+            if tr['type'] == 'sell':
+                break
+            else:
+                sum = tr['price']
+        return sum
+
+    def cancel_orders(self, order_ids):
+        method = "/exchange/cancellimit"
+        for od in order_ids:
+            data = OrderedDict(sorted([('currencyPair', od['pair']), ('orderId', od['id'])]))
+            response = self.post_request(method, data)
+            value = json.loads(response)
+            if value['cancelled'] == True:
+                print('успешно отменен ордер #', od['id'], 'объёмом', value['quantity'], od['pair'])
+            else:
+                print('ошибка отмены ордера #', od['id'], od['pair'])
+
+    def buy_currency(self, pair, quantity, price):
+        method = "/exchange/buylimit"
+        data = OrderedDict(sorted([('currencyPair', pair), ('price', price), ('quantity', quantity)]))
+        response = self.post_request(method, data)
+        value = json.loads(response)
+        if value['success'] == True:
+            print('успешно создан ордер на покупку', str(quantity), pair, 'по курсу', str(price), ' #:',
+                  value['orderId'])
+        else:
+            print(value)
+
+    def sell_currency(self, pair, quantity, price):
+        method = "/exchange/selllimit"
+        data = OrderedDict(sorted([('currencyPair', pair), ('price', price), ('quantity', quantity)]))
+        response = self.post_request(method, data)
+        value = json.loads(response)
+        if value['success'] == True:
+            print('успешно создан ордер на продажу', str(quantity), pair, 'по курсу', str(price), ' #:',
+                  value['orderId'])
+        else:
+            print(value)
