f002cd24eff160cba39bd6306480c9daad525033
Vasilii <basswan@yandex.ru>
2017-08-23 13:01:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,73 +1,77 @@
 from livecoin_api import trade_api
 import settings
 
-trade_bot = trade_api()
+trade_bot = trade_api(settings.proxy_sett)
 
 order_ids = trade_bot.get_openorders()
 
 ex_list = trade_bot.get_partiallyorders()
+print(ex_list)
+down_list = trade_bot.get_down_list()
+
+for dl in down_list:
+    ex_list.append(dl + trade_bot.get_btc_ex())
+
+ex_list.append('BCH' + trade_bot.get_btc_ex())
 
 trade_bot.cancel_orders(order_ids)
 
 balances = trade_bot.get_balanses()
 print(balances)
 
-btc_balance = balances['BTC']
+btc_balance = trade_bot.get_btc_balance(balances)
 
 for b in balances:
-    if b != 'BTC':
-        ex_list.append(b + '/BTC')
+    if str(b).upper() != 'BTC' and ((b + trade_bot.get_btc_ex()) not in ex_list):
+        ex_list.append(b + trade_bot.get_btc_ex())
         # sell this currency
-        buys_pr = trade_bot.get_buy_price(b + '/BTC')
+        buys_pr = trade_bot.get_buy_price(b + trade_bot.get_btc_ex())
         if (buys_pr) == 0:
-            print('Not info about ' + b)
+            print('Не смог найти данные о покупке ' + str(b).upper())
             continue
-        dt = trade_bot.get_coin_details(b + '/BTC')
+        dt = trade_bot.get_coin_details(b + trade_bot.get_btc_ex())
         if not trade_bot.is_Error(dt):
             dt = trade_bot.get_currency_info(dt)
             min_ask = trade_bot.get_minask(dt) - settings.sell_step
-            if buys_pr * (1 + settings.min_delta + 0.005) < min_ask:
-                trade_bot.sell_currency(b + '/BTC', "{0:.8f}".format(balances[b]), "{0:.8f}".format(min_ask))
+            if buys_pr * (1 + settings.min_delta) < min_ask:
+                trade_bot.sell_currency(b + trade_bot.get_btc_ex(), "{0:.8f}".format(balances[b]), "{0:.8f}".format(min_ask))
             else:
-                print('Wait in ' + b)
-
-ex_list.append('MCO/BTC')
-ex_list.append('BCH/BTC')
+                print('Прогорели в ' + b)
 
 order_size = trade_bot.get_min_order_size(settings.min_order_mult)
-max_num_orders = int(btc_balance * 0.99 / order_size)
+max_num_orders = int((btc_balance * 0.99) / order_size)
 
 pair_ls = trade_bot.get_coin_list()
 
-d = {}
+rg_list = []
 
 # get pair list for analisis
 for co in pair_ls:
     vol = trade_bot.get_volume(co)
     sym = trade_bot.get_pair(co)
-    lo = trade_bot.get_low(co)
-    if (vol > settings.min_volume) and (lo > settings.min_ask) and trade_bot.is_btc(sym) and (sym not in ex_list):
-        d[sym] = vol
-
-rg_list = []
-
-# get rang for all coins in list
-for co in d:
-    dt = trade_bot.get_coin_details(co)
-    if not trade_bot.is_Error(dt):
-        dt = trade_bot.get_currency_info(dt)
-        min_ask = trade_bot.get_minask(dt)
-        max_bid = trade_bot.get_maxbid(dt)
-        pr = (min_ask - max_bid - settings.buy_step - settings.sell_step) / (max_bid + settings.buy_step)
-        rang = pr * d[co]
+    low = trade_bot.get_low(co)
+    high = trade_bot.get_high(co)
+    best_bid = trade_bot.get_best_bid(co)
+    best_ask = trade_bot.get_best_ask(co)
+    if high > low:
+        dev = (high - low) / low
+    else:
+        dev = settings.max_dev
+    if (vol > settings.min_volume) and (low > settings.min_ask) and trade_bot.is_btc(sym) and (sym not in ex_list) and (
+                dev < settings.max_dev):
+        pr = (best_ask - best_bid - settings.buy_step - settings.sell_step) / (best_bid + settings.buy_step)
+        rang = pr * vol
         if (pr > settings.min_profit) and (pr < settings.max_profit):
-            rg_list.append({'rang': rang, 'symbol': trade_bot.get_pair(dt)})
+            rg_list.append({'rang': rang, 'symbol': sym})
 
 rg_list.sort(key=lambda i: i['rang'], reverse=True)
 
 if max_num_orders > len(rg_list):
     max_num_orders = len(rg_list)
-    order_size = (btc_balance  * 0.99)/ max_num_orders
+    order_size = (btc_balance * 0.99) / max_num_orders
+
+    while (order_size * max_num_orders) > (btc_balance * 0.99):
+        order_size -= 0.00000001
 
 i = 0
 for k in rg_list:
@@ -76,10 +80,7 @@ for k in rg_list:
         if not trade_bot.is_Error(dt):
             dt = trade_bot.get_currency_info(dt)
             max_bid = trade_bot.get_maxbid(dt) + settings.buy_step
-            quantity = order_size/max_bid
-            while (quantity > 0.00000001) and ((quantity*max_bid) > order_size):
-                quantity -= 0.00000001
-            print(k['symbol'], "{0:.8f}".format(quantity), "{0:.8f}".format(max_bid), k['rang'])
+            quantity = order_size / max_bid
             trade_bot.buy_currency(k['symbol'], "{0:.8f}".format(quantity), "{0:.8f}".format(max_bid))
     else:
         break
--- settings.py
+++ settings.py
@@ -1,9 +1,30 @@
 default_loss_time = 14  # in days
-min_volume = 0.1  # min volume
+min_volume = 2  # min volume
 min_ask = 0.000005  # min ask
 min_profit = 0.04
-max_profit = 0.25
-min_delta = 0.04
-min_order_mult = 1.4
+max_profit = 0.3
+min_delta = 0.02
+min_order_mult = 1.5
 buy_step = 0.00000001
 sell_step = 0.00000001
+max_dev = 0.25
+
+proxy = 'maker.garant.ru:3128'
+proxyhost = 'maker.garant.ru'
+proxyport = 3128
+proxy_used = True
+
+proxyDict = {
+    "http": proxy,
+    "https": proxy,
+    "ftp": proxy
+}
+
+proxy_sett = {
+    'proxy_url': proxy,
+    'proxyhost': proxyhost,
+    'proxyport': proxyport,
+    'proxy_used': proxy_used,
+    'proxyDict': proxyDict
+
+}
